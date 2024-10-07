22ea5500ae0ac6812cb1eef4e034cc04d63909a9
AviraLS22 <aviralsharma.sharma2205@gmail.com>
2024-10-07 21:54:31 +0530

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import ChatWithGemini from './api/ChatWithGeminitsx'
+
+
+function MyApp() {
+  return (<>
+        <ChatWithGemini/>
+  </>)
+}
+
+export default MyApp
